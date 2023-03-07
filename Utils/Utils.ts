8a03e5a8dedaacb013aaf1b30e8d354c01464a65
zjsjmvn <87652897@qq.com>
2023-03-07 22:22:43 +0800

    工具添加block

--- Utils/Utils.ts
+++ Utils/Utils.ts
@@ -591,5 +591,24 @@ export default class Utils {
     public static isValidNumber(value) {
         return !isNaN(parseFloat(value)) && isFinite(value);
     }
+
+    public static block(seconds: number) {
+        return function (target, methodName: string, descriptor: PropertyDescriptor) {
+            let oldMethod = descriptor.value
+            let isBlock = false
+            descriptor.value = function (...args: any[]) {
+                if (isBlock) {
+                    console.info('ii.Util.block >> blocking')
+                    return
+                }
+                isBlock = true
+                setTimeout(() => {
+                    isBlock = false
+                }, seconds * 1000)
+                oldMethod.apply(this, args)
+            }
+            return descriptor
+        }
+    }
 }
 

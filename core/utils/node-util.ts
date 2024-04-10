cf04ced532c5f7bad035d58c0c43d4deab4e67ed
mmgg <87652897@qq.com>
2024-04-10 17:36:55 +0800

    New： 添加ecs文档

--- core/utils/node-util.ts
+++ core/utils/node-util.ts
@@ -84,3 +84,135 @@ export default class NodeUtil {
 
 
 }
+
+declare module 'cc' {
+    interface Node {
+        /**x轴坐标 */
+        x: number,
+        /**y轴坐标 */
+        y: number,
+        /**z轴坐标 */
+        z: number,
+        /**x轴缩放 */
+        scaleX: number,
+        /**y轴缩放 */
+        scaleY: number,
+        /**z轴缩放 */
+        scaleZ: number,
+        /**x轴旋转 */
+        angleX: number,
+        /**y轴旋转 */
+        angleY: number,
+        /**z轴旋转, 等同于angle */
+        angleZ: number,
+    }
+}
+
+Object.defineProperty(Node.prototype, 'x', {
+    get: function () {
+        return this.position.x;
+    },
+    set: function (v: number) {
+        this.position.set(v, this.position.y, this.position.z);
+        this.setPosition(this.position);
+    },
+    enumerable: true,
+    configurable: true,
+});
+
+Object.defineProperty(Node.prototype, 'y', {
+    get: function () {
+        return this.position.y;
+    },
+    set: function (v: number) {
+        this.position.set(this.position.x, v, this.position.z);
+        this.setPosition(this.position);
+    },
+    enumerable: true,
+    configurable: true,
+});
+
+Object.defineProperty(Node.prototype, 'z', {
+    get: function () {
+        return this.position.z;
+    },
+    set: function (v: number) {
+        this.position.set(this.position.x, this.position.y, v);
+        this.setPosition(this.position);
+    },
+    enumerable: true,
+    configurable: true,
+});
+
+Object.defineProperty(Node.prototype, 'scaleX', {
+    get: function () {
+        return this.scale.x;
+    },
+    set: function (v: number) {
+        this.scale.set(v, this.scale.y, this.scale.z);
+        this.setScale(this.scale);
+    },
+    enumerable: true,
+    configurable: true,
+});
+
+Object.defineProperty(Node.prototype, 'scaleY', {
+    get: function () {
+        return this.scale.y;
+    },
+    set: function (v: number) {
+        this.scale.set(this.scale.x, v, this.scale.z);
+        this.setScale(this.scale);
+    },
+    enumerable: true,
+    configurable: true,
+});
+
+Object.defineProperty(Node.prototype, 'scaleZ', {
+    get: function () {
+        return this.scale.z;
+    },
+    set: function (v: number) {
+        this.scale.set(this.scale.x, this.scale.y, v);
+        this.setScale(this.scale);
+    },
+    enumerable: true,
+    configurable: true,
+});
+
+Object.defineProperty(Node.prototype, 'angleX', {
+    get: function () {
+        return this.eulerAngles.x;
+    },
+    set: function (v: number) {
+        this.eulerAngles.set(v, this.eulerAngles.y, this.eulerAngles.z);
+        this.setRotationFromEuler(this.eulerAngles);
+    },
+    enumerable: true,
+    configurable: true,
+});
+
+Object.defineProperty(Node.prototype, 'angleY', {
+    get: function () {
+        return this.eulerAngles.y;
+    },
+    set: function (v: number) {
+        this.eulerAngles.set(this.eulerAngles.x, v, this.eulerAngles.z);
+        this.setRotationFromEuler(this.eulerAngles);
+    },
+    enumerable: true,
+    configurable: true,
+});
+
+Object.defineProperty(Node.prototype, 'angleZ', {
+    get: function () {
+        return this.eulerAngles.z;
+    },
+    set: function (v: number) {
+        this.eulerAngles.set(this.eulerAngles.x, this.eulerAngles.y, v);
+        this.setRotationFromEuler(this.eulerAngles);
+    },
+    enumerable: true,
+    configurable: true,
+});
+

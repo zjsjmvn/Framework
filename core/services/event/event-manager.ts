ed4e28c7a5c1de310037f135270a12bcfec34a5a
Mg <87652897@qq.com>
2024-08-26 12:57:34 +0800

    New： 事件添加移除所有指定类型

--- core/services/event/event-manager.ts
+++ core/services/event/event-manager.ts
@@ -69,6 +69,9 @@ export class EventManager {
             }
         }
     }
+    removeAllSpecifiedEventListeners(eventName: string) {
+        this._eventListeners.delete(eventName);
+    }
     off(eventName: string, callBack: Function, target?: any) {
         return this.removeEventListener(eventName, callBack, target);
     }
